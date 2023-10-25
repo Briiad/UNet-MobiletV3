ca4cfb7adfa6c080c3ac542d130a0ba74d7687d1
Brian Aditya <brianadit33@gmail.com>
2023-10-25 16:44:38 +0700

    test model

--- run.py
+++ run.py
@@ -1,18 +1,15 @@
 import numpy as np
 import onnxruntime as rt
 import cv2
-from PIL import Image
-
 import torch
 
 # GPU
 print("CUDA is available: ", torch.cuda.is_available())
 
-def preprocess(path, input_size=(256, 192), mean=[0.485, 0.485, 0.485], std=[0.229, 0.229, 0.229]):
-    img = Image.open(path)
-    img = img.resize(input_size)
-
-    img_np = np.array(img) / 255.0
+def preprocess(frame, input_size=(256, 192), mean=[0.485, 0.485, 0.485], std=[0.229, 0.229, 0.229]):
+    img_resized = cv2.resize(frame, input_size)
+    
+    img_np = img_resized / 255.0
     img_np = (img_np - mean) / std
 
     tensor = np.transpose(img_np, (2, 0, 1))
@@ -20,17 +17,15 @@ def preprocess(path, input_size=(256, 192), mean=[0.485, 0.485, 0.485], std=[0.2
 
     return tensor
 
-def infer_depth(model_path, input_tensor):
-    sess = rt.InferenceSession(model_path)
-
+def infer_depth(sess, input_tensor):
     input_name = sess.get_inputs()[0].name
-
     outputs = sess.run(None, {input_name: input_tensor})
 
     return outputs[0]
 
 def main():
     model = './model.onnx'
+    sess = rt.InferenceSession(model)  # Load the model once outside the loop
 
     # Gstreamer Pipeline on Webcam
     cap = cv2.VideoCapture("v4l2src device=/dev/video0 ! video/x-raw,format=YUY2,width=640,height=480,framerate=30/1 ! videoconvert ! video/x-raw,format=BGR ! appsink")
@@ -40,7 +35,9 @@ def main():
 
         input_tensor = preprocess(frame)
 
-        depth = infer_depth(model, input_tensor)
+        depth = infer_depth(sess, input_tensor)
+
+        cv2.imshow('frame', depth)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
