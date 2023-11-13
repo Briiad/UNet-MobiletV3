7a144a6fd2684bfc73ad9f2dafeb79fce7835aa1
Brian Aditya <brianadit33@gmail.com>
2023-11-13 11:22:37 +0700

    add estimate distance

--- run.py
+++ run.py
@@ -16,11 +16,20 @@ while cap.isOpened():
     if not ret:
         break
     
+    # Depth Estimation
     depth_map = depth_estimator(frame)
     color_depth = depth_estimator.draw_depth()
 
-    # combined_depth = np.hstack((frame, color_depth))
+    # Square Bounding Box To measure distance, located at the center of the image
+    cv2.rectangle(color_depth, (int(frame.shape[1]/2-50), int(frame.shape[0]/2-50)), (int(frame.shape[1]/2+50), int(frame.shape[0]/2+50)), (0, 255, 0), 2)
 
+    # Estimate distance
+    distance = depth_map[int(frame.shape[0]/2), int(frame.shape[1]/2)]
+
+    # Display Distance
+    cv2.putText(color_depth, "Distance: {:.2f} cm".format(distance), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+    # Display Depth
     cv2.imshow("Depth", color_depth)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
