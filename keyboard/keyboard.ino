3bc04f7af0cf1bc87a4e37b2d54f8aac46842f93
Brian Aditya <brianadit33@gmail.com>
2023-11-14 11:32:01 +0700

    fix

--- keyboard/keyboard.ino
+++ keyboard/keyboard.ino
@@ -61,22 +61,42 @@ void right(){
 void loop(){
   if (Serial.available() > 0){
     char input = Serial.read();
+  }
 
-    switch(input){
+  switch(input){
       case 'w':
         forward();
+        state = 1;
         break;
       case 's':
         stop();
+        state = 0;
         break;
       case 'a':
         left();
+        state = 2;
         break;
       case 'd':
         right();
+        state = 3;
         break;
       default:
         break;
     }
-  }
+
+    // PWM Write
+    if (state != last_state){
+      spam_counter = 0;
+    }
+
+    if (spam_counter < 10){
+      analogWrite(PWM_0, pmw_speed[0]);
+      analogWrite(PWM_1, pmw_speed[1]);
+      analogWrite(PWM_2, pmw_speed[2]);
+      analogWrite(PWM_3, pmw_speed[3]);
+      spam_counter++;
+    }
+
+    last_state = state;
+    delay(10);
 }
